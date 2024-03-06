6dc5a0aeaf7f952b9014a991c714555d2911a021
Ani <axtinemvsn@gmail.com>
2024-03-05 22:13:56 -0800

    Included links. Ready to deploy first version!

--- deploy_to_node-alpha-02.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Copying /home/ani/Desktop/Website/ to ubuntu@192.168.50.202:/var/www/anicmovsesian.com/"
-sshpass -p "D3v310p3r5!" scp -r /home/ani/Desktop/Website/* ubuntu@192.168.50.202:/var/www/anicmovsesian.com/ \
-&& echo "Copied Successfully. Check http://192.168.50.202"
